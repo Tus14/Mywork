089170052bbd9e4b8681ac1d1c7efc4ba1bdd220
Татьяна Иванова <t-tarasenko@mail.ru>
2025-11-07 16:49:33 +0300

    add new function dict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
-from src.widget import mask_account_card, get_date
-from src.processing import filter_by_state
+from src.processing import filter_by_state, sort_by_date
+from src.widget import get_date, mask_account_card
 
 if __name__ == "__main__":
     card_mask = get_mask_card_number("7000792289606361")
@@ -20,8 +20,24 @@ if __name__ == "__main__":
 
 
 if __name__ == "__main__":
-    new_list = filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                                {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                                {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                                {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
+    new_list = filter_by_state(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
     print(new_list)
+
+
+if __name__ == "__main__":
+    new_dict = sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+    print(new_dict)
